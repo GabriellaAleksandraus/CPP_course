e93628f8bcbd0ab79165f276b23588a35d27f02c
Gabriella <gabriella@aleksandraus.se>
2024-10-15 15:18:16 +0200

    challanges

--- Section 9/ForLoop.cpp
+++ Section 9/ForLoop.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <format>
 
 using namespace std;
+
 
 int main() {
 
     int sum {};
 
-    for (int i = 1; i <= 15; i = i + 2) {
-        cout << i << endl;
-        sum = sum + i;
-        cout << sum << endl;
+    // for (int i = 1; i <= 15; i = i + 2) {
+    //     cout << i << endl;
+    //     sum = sum + i;
+    //     cout << sum << endl;
+    // }
+
+    for (int i = 1; i <=5; i = i + 1) {
+        cout << std::format("{}", vec)
+
     }
 
 }

60e1163028f8556291f2763388f4f8ed97f39fc9
Gabriella <gabriella@aleksandraus.se>
2024-10-30 15:32:41 +0100

    function challenge

---
+++ Section 11/FunctionChallange.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void displayMenu() {
+
+    std::cout << "P - Print numbers" << std::endl;
+    std::cout << "A - Add a number" << std::endl;
+    std::cout << "M - Display mean of the numbers" << std::endl;
+    std::cout << "S - Display the smallest number" << std::endl;
+    std::cout << "L - Display the largest number" << std::endl;
+    std::cout << "Q - Quit" << std::endl;
+    std::cout << "\nEnter you choice: " << std::endl;
+
+}
+
+double calcMean(std::vector <int> collectionOfIntegers) {
+    double sum{};
+    double mean{};
+    
+    for (int number : collectionOfIntegers) {
+        sum += number ;
+    }
+    mean = sum / collectionOfIntegers.size();
+
+    return mean;
+
+}
+
+char parseSelection(char selection) {
+
+    char selectionUpper = toupper(selection);
+    //std::cout << selectionUpper;
+    return selectionUpper;
+
+}
+
+void printNumbers(std::vector <int> collectionOfIntegers) {
+    if (collectionOfIntegers.empty()) {
+        std::cout << "\n[] - the list is empty\n" << std::endl;
+    }
+    else {
+        std::cout << "[ ";
+        for (int number : collectionOfIntegers) {
+            std::cout << number << " ";
+            }
+            std::cout << "]\n" << std::endl;
+        }
+
+}
+
+void addNumbers(std::vector <int>& collectionOfIntegers, int number) {
+    collectionOfIntegers.push_back(number);
+    std::cout << number << " added." << std::endl;
+}
+
+void displayMean(std::vector <int>& collectionOfIntegers) {
+
+    if (collectionOfIntegers.empty()) {
+        std::cout << "\nUnable to calculate the mean. The list is empty\n" << std::endl;
+    }
+    else {
+        std::cout << "\nMean: " << calcMean(collectionOfIntegers) << "\n" << std::endl;
+    }
+}
+
+void displaySmallestNumber(std::vector <int>& collectionOfIntegers) {
+    if (collectionOfIntegers.empty()) {
+        std::cout << "\nThe list is empty\n" << std::endl;
+    }
+    else {
+        auto smallest_number
+            = min_element(collectionOfIntegers.begin(), collectionOfIntegers.end());
+
+        std::cout << "\nThe smallest number is: " << *smallest_number << "\n" << std::endl;
+    }
+}
+
+void displayLargestNumber(std::vector <int>& collectionOfIntegers) {
+    if (collectionOfIntegers.empty()) {
+        std::cout << "\nThe list is empty\n" << std::endl;
+    }
+    else {
+        auto largest_number
+            = max_element(collectionOfIntegers.begin(), collectionOfIntegers.end());
+
+        std::cout << "\nThe largest number is: " << *largest_number << "\n" << std::endl;
+    }    
+}
+
+void quit() {
+    std::cout << "Goodbye!"<< std::endl;
+    exit(0);    
+}
+
+
+
+
+int main() {
+
+    char selection {};
+    std::vector <int> collectionOfIntegers{};
+    int number{};
+
+
+    do {
+
+        displayMenu();
+        std::cin >> selection;
+        selection = parseSelection(selection);
+
+        if (selection == 'P') {
+            printNumbers(collectionOfIntegers);
+        }
+        else if (selection == 'A') {
+            std::cin >> number;
+            addNumbers(collectionOfIntegers, number);
+        }
+        else if (selection == 'M') {
+            displayMean(collectionOfIntegers);
+        }
+        else if (selection == 'S') {
+            displaySmallestNumber(collectionOfIntegers);
+        }
+        else if (selection == 'L') {
+            displayLargestNumber(collectionOfIntegers);
+        }
+        else if (selection == 'Q') {
+            quit();
+        }
+        else {
+            std::cout << "\nUnknown selection, please try again\n" << std::endl;
+        }
+
+    } while (selection != 'Q');
+
+    return 0;
+
+}
